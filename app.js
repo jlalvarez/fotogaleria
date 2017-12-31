4cab742ffecd83afb4e972c3b785dbcb7341b6d6
JL Alvarez <alvarez@uhu.es>
2017-12-31 10:18:29 +0000

    Actualizando v 0.0.2

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require('express');
-
 var app = express();
 
 app.use("/public", express.static('public'));
 app.set('view engine', 'pug');
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello José Luis!' });
+  console.log(process.env.PORT);
+  res.render('index', { title: 'Hola', message: 'Hello José Luis!' });
 });
 
-app.listen(8080);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
 
-// app.listen(process.env.PORT, process.env.IP);
+//app.listen(8080);
+//app.listen(process.env.PORT); //, process.env.IP);
